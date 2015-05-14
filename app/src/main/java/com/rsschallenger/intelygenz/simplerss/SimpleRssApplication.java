cb89e8ccd4bf5d7e7209bd2a8bffef7961439891
unknown <montero1331@hotmail.com>
2015-05-14 17:54:23 +0200

    Base aplication defined

---
+++ app/src/main/java/com/rsschallenger/intelygenz/simplerss/SimpleRssApplication.java
@@ -0,0 +1,15 @@
+package com.rsschallenger.intelygenz.simplerss;
+
+import android.app.Application;
+
+/**
+ * Created by Jose Luis on 14/05/2015.
+ */
+public class SimpleRssApplication extends Application {
+
+    @Override
+    public void onCreate(){
+
+    }
+
+}
